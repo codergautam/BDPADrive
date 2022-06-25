9bc38b293a0c3adbccee62cbc1deb62d64cc45e5
Coder Gautam <gautamgxtv@gmail.com>
2022-06-24 20:42:18 -0500

    server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log("hello world!");
+const express = require('express')
+const app = express()
+const port = 3000
 
-jhvdfjghgjfdhg
+app.use("/", express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
